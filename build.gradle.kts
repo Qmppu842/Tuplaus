0d1cde3b7b8b4835191fa6c0ab9adb217cdcc1dc
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2023-05-31 15:57:15 +0300

    Added exposed db to dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,15 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val exposed_version: String by project
+val h2_version: String by project
 
 plugins {
     kotlin("jvm") version "1.8.21"
     id("io.ktor.plugin") version "2.3.0"
 
     id("info.solidsoft.pitest") version "1.9.11"
+    kotlin("plugin.serialization") version "1.8.20"
 }
 
 group = "io.qmppu842"
@@ -31,6 +34,15 @@ dependencies {
 
     testImplementation("io.ktor:ktor-server-test-host:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
+
+    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("com.h2database:h2:$h2_version")
 }
 
 
